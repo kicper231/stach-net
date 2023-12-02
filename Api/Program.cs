84e8859653dbc1f75671f2b8b795d2bb4e916ca4
kicper231 <krol.kacper.pg2@gmail.com>
2023-12-02 20:35:10 +0100

    komentarze lekka zmiana

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,34 +5,40 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Dodaje us³ugi do kontenera.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // Dodaje us³ugi obs³ugi kontrolerów MVC do aplikacji
+builder.Services.AddEndpointsApiExplorer(); // Umo¿liwia eksploracjê endpointów API, przydatne dla Swagger
+builder.Services.AddSwaggerGen(); // Dodaje generator Swaggera, który dostarcza UI do testowania API i dokumentacji
 
-builder.Services.AddScoped<ICustomerRepository, DbCustomerRepository>();
+// Dodanie us³ugi scoped dla ICustomerRepository, która bêdzie u¿ywaæ DbCustomerRepository do swojej implementacji
+builder.Services.AddScoped<ICustomerRepository, FakeCustomerRepository>();
 
+// Konfiguracja factory do tworzenia DbContext, konkretnie ShopperContext, u¿ywaj¹c SQLite jako bazy danych
 builder.Services.AddDbContextFactory<ShopperContext>(options =>
     options.UseSqlite("Data Source=shopper.db"));
-//stara barana start
-// builder.Services.AddHostedService<DbCreationalService>();
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Odkomentuj, aby dodaæ us³ugê hostowan¹, która uruchomi DbCreationalService przy starcie
+// builder.Services.AddHostedService<DbCreationalService>();
+
+var app = builder.Build(); // Buduje aplikacjê webow¹
+
+// Konfiguruje pipeline ¿¹dañ HTTP.
+
+// W œrodowisku developerskim u¿yj Swaggera do dokumentacji i testowania API
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Dodaje middleware do przekierowywania ¿¹dañ HTTP na HTTPS
 
-app.UseAuthorization();
+app.UseAuthorization(); // Dodaje middleware do autoryzacji
 
-app.MapControllers();
+app.MapControllers(); // Mapuje trasy do akcji kontrolerów
 
+// Ta niestandardowa metoda rozszerzenia (nie jest czêœci¹ ASP.NET Core) prawdopodobnie s³u¿y do upewnienia siê, ¿e baza danych jest tworzona przy starcie aplikacji.
 app.CreateDatabase<ShopperContext>();
 
-app.Run();
+app.Run(); // Uruchamia aplikacjê
