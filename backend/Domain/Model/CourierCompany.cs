d1b8570d891b5e102faa26b844829d53335fb4ce
kicper231 <krol.kacper.pg2@gmail.com>
2023-12-10 00:17:54 +0100

    dodanie modelu danych (raczej błedny trzeba dodac POCO lub DTO)

---
+++ backend/Domain/Model/CourierCompany.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Domain.Model;
+
+
+public class CourierCompany : BaseEntity
+{
+    public int CourierCompanyId { get; set; }
+    public string Name { get; set; }
+    public string ContactInfo { get; set; }
+
+}
---
+++ backend/Domain/Model/Offer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model;
+
+
+public class Offer : BaseEntity
+{
+    public int OfferId { get; set; }
+    public int DeliveryRequestId { get; set; }
+    public DeliveryRequest DeliveryRequest { get; set; }
+    public int CourierCompanyId { get; set; }
+    public CourierCompany CourierCompany { get; set; }
+    public decimal Price { get; set; }
+    public DateTime OfferValidity { get; set; }
+    public OfferStatus OfferStatus { get; set; }
+}
+
+public enum OfferStatus
+{
+    Available,
+    Expired
+}
+
