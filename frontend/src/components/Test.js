4805f2d376fe8a694b2a9afa2a32a756572c51bc
Mateusz Tkaczyk <mateusztk@onet.eu>
2023-12-28 17:23:41 +0100

    Add test endpoint

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -1,8 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 
 export function Test() {
   const [activeUsers, setActiveUsers] = useState(null);
+  const [data, setData] = useState({});
 
   const { getAccessTokenSilently } = useAuth0();
 
@@ -10,7 +14,6 @@ export function Test() {
     const callApi = async () => {
       try {
         const token = await getAccessTokenSilently();
-        const domain = "dev-6i665fwgjuvholkh.us.auth0.com";
         const response = await fetch(
           `https://${domain}/api/v2/stats/active-users/`,
           {
@@ -36,9 +39,47 @@ export function Test() {
     callApi();
   });
 
+  useEffect(() => {
+    const sendDataWithFetch = async () => {
+      try {
+        const response = await fetch("http://localhost:5157/test_post_fetch", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+          console.log("Form submitted successfully!");
+        } else {
+          console.error("Form submission failed:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error sending form data:", error);
+      }
+    };
+
+    const sendDataWithAxios = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5157/test_post_axios",
+          data
+        );
+        console.log("Data sent successfully:", response.data);
+      } catch (error) {
+        console.error("Error sending data to the server:", error);
+      }
+    };
+
+    setData({ id: 1, string: "abc" });
+    sendDataWithFetch();
+    sendDataWithAxios();
+  }, [data]);
+
   return (
     <div>
-      <h2>Auth0 Active Users</h2>
+      <h1>TEST</h1>
       {activeUsers ? (
         <p>Number of Active Users: {activeUsers}</p>
       ) : (
