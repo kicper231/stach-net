215d2e72fb044b6d63160d4896b1fc7741de05e2
Kapiszon <128547863+kicper231@users.noreply.github.com>
2024-01-04 22:03:21 +0100

    Http changes

--- backend/Api/Service/htppservisetest.cs
+++ backend/Api/Service/htppservisetest.cs
@@ -4,28 +4,30 @@ namespace Api.Service;
 
 
 
-public interface IHttpService
+public interface IOfferService
 {
-    Task<string> GetAsync(string url);
-     Task<DeliveryRespondDTO> PostDeliveryRequestAsync(string url, DeliveryRequestDTO requestDTO);
+   // Task<string> GetAsync(string url);  
+     Task<DeliveryRespondDTO> GetOffer(string url, DeliveryRequestDTO requestDTO);
 }
-public class HttpService : IHttpService
+public class OfferService : IOfferService
 {
     private readonly HttpClient _httpClient;
 
-    public HttpService(HttpClient httpClient)
+    public OfferService(HttpClient httpClient)
     {
         _httpClient = httpClient;
+        //_httpClient.BaseAddress=
+        // do zmiany //secret key 
     }
 
-    public async Task<string> GetAsync(string url)
-    {
-        var response = await _httpClient.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync();
-    }
+    //public async Task<string> GetAsync(string url)
+    //{
+    //    var response = await _httpClient.GetAsync(url);
+    //    response.EnsureSuccessStatusCode();
+    //    return await response.Content.ReadAsStringAsync();
+    //}
 
-    public async Task<DeliveryRespondDTO> PostDeliveryRequestAsync(string url, DeliveryRequestDTO requestDTO)
+    public async Task<DeliveryRespondDTO> GetOffer(string url, DeliveryRequestDTO requestDTO)
     {
         var response = await _httpClient.PostAsJsonAsync(url, requestDTO);
         response.EnsureSuccessStatusCode();
