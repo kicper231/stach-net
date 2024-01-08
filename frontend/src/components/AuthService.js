752bb94cb273145372fb571bd3d689a23e4402e7
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-01-08 16:04:06 +0100

    Move checking roles logic to AuthService

---
+++ frontend/src/components/AuthService.js
@@ -0,0 +1,15 @@
+const rolesUri = "https://stachnet.azurewebsites.net//roles";
+
+export const AuthService = {
+  isCourier: (user) => {
+    return (
+      user && Array.isArray(user[rolesUri]) && user[rolesUri].includes("Curier")
+    );
+  },
+
+  isClient: (user) => {
+    return (
+      user && Array.isArray(user[rolesUri]) && user[rolesUri].includes("Client")
+    );
+  },
+};
