311d5a558ea8207312c8aa710c95635617ba645e
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-01-09 00:58:10 +0100

    Change package dimensions and address properties, refactor RequestInquiry

--- frontend/src/components/RequestInquiry.js
+++ frontend/src/components/RequestInquiry.js
@@ -1,42 +1,36 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-//const serverUrl = process.env.SERVER_URL;
-
 export function RequestInquiry() {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
+    userAuth0: "TOKEN",
     package: {
-      width: 2,
-      height: 3,
-      length: 1,
-      // dimensionUnit: "Meters/Inches",
+      width: 1.5,
+      height: 1,
+      length: 3,
       weight: 10,
-      // weightUnit: "weight",
-      priority: false,
-      weekendDelivery: false,
     },
     sourceAddress: {
-      houseNumber: "string",
-      apartmentNumber: "string",
-      street: "Lubelska ",
+      houseNumber: "1",
+      apartmentNumber: "",
+      street: "Lubelska",
       city: "Lublin",
       postalCode: "20-000",
       country: "Polska",
     },
     destinationAddress: {
-      houseNumber: "string",
-      apartmentNumber: "string",
-      street: "Nowowiejska ",
+      houseNumber: "13",
+      apartmentNumber: "304",
+      street: "Nowowiejska",
       city: "Warszawa",
       postalCode: "000-00",
       country: "Polska",
     },
-    deliveryDate: "2024-2-29",
-    vipPackage: true,
-
-    isCompany: true,
+    deliveryDate: "2024-02-29",
+    priority: false,
+    weekendDelivery: false,
   });
 
   const handleChange = (e, key = null) => {
@@ -48,12 +42,6 @@ export function RequestInquiry() {
 
     if (e.target.name === "priority") {
       value = value === "option1" ? true : false;
-    }
-    if (e.target.name === "vipPackage") {
-      value = value === "option1" ? true : false;
-    }
-    if (e.target.name === "IsCompany") {
-      value = value === "option3" ? true : false;
     }
 
     if (key === null) {
@@ -72,287 +60,226 @@ export function RequestInquiry() {
     }
   };
 
-  const handleSubmit = async (e) => {
-    //e.preventDefault();
-    //  console.log(JSON.stringify(formData));
+  const handleSend = async () => {
+    // Send data logic
+
     navigate("/delivery-request/offers", { state: { formData: formData } });
   };
 
   return (
     <div className="overflow">
-      <form onSubmit={handleSubmit}>
-        <p>Package</p>
+      <p>Package details:</p>
+      <label>
+        width (m):
+        <input
+          type="number"
+          name="width"
+          value={formData.package.width}
+          onChange={(e) => handleChange(e, "package")}
+        />
+      </label>
+      <br />
+      <label>
+        height (m):
+        <input
+          type="number"
+          name="height"
+          value={formData.package.height}
+          onChange={(e) => handleChange(e, "package")}
+        />
+      </label>
+      <br />
+      <label>
+        length (m):
+        <input
+          type="number"
+          name="length"
+          value={formData.package.length}
+          onChange={(e) => handleChange(e, "package")}
+        />
+      </label>
+      <br />
+      <label>
+        weight (kg):
+        <input
+          type="number"
+          name="weight"
+          value={formData.package.weight}
+          onChange={(e) => handleChange(e, "package")}
+        />
+      </label>
 
-        <label>
-          width:
-          <input
-            type="text"
-            name="width"
-            value={formData.package.width}
-            onChange={(e) => handleChange(e, "package")}
-          />
-        </label>
-        <br />
-        <label>
-          height:
-          <input
-            type="text"
-            name="dimensions"
-            value={formData.package.height}
-            onChange={(e) => handleChange(e, "package")}
-          />
-        </label>
-        <br />
+      <p>Source address:</p>
+      <label>
+        street:
+        <input
+          type="text"
+          name="street"
+          value={formData.sourceAddress.street}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        house number:
+        <input
+          type="number"
+          name="houseNumber"
+          value={formData.sourceAddress.houseNumber}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        apartment number:
+        <input
+          type="number"
+          name="apartmentNumber"
+          value={formData.sourceAddress.apartmentNumber}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        city:
+        <input
+          type="text"
+          name="city"
+          value={formData.sourceAddress.city}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        postal code:
+        <input
+          type="text"
+          name="postalCode"
+          value={formData.sourceAddress.postalCode}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        country:
+        <input
+          type="text"
+          name="country"
+          value={formData.sourceAddress.country}
+          onChange={(e) => handleChange(e, "sourceAddress")}
+        />
+      </label>
 
-        <label>
-          length:
-          <input
-            type="text"
-            name="length"
-            value={formData.package.length}
-            onChange={(e) => handleChange(e, "package")}
-          />
-        </label>
-        <br />
-        <label>
-          Weight (kg):
-          <input
-            type="number"
-            name="weight"
-            value={formData.package.weight}
-            onChange={(e) => handleChange(e, "package")}
-          />
-        </label>
-        <br />
-        <label>
-          Priority:
-          <br />
-          <label>
-            <input
-              type="radio"
-              name="priority"
-              value="option1"
-              checked={formData.package.priority === true}
-              onChange={(e) => handleChange(e, "package")}
-              required
-            />
-            High
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="priority"
-              value="option2"
-              checked={formData.package.priority === false}
-              onChange={(e) => handleChange(e, "package")}
-              required
-            />
-            Low
-          </label>
-        </label>
-        <br />
-        <label>
-          Delivery at a weekend:
-          <input
-            type="checkbox"
-            name="weekendDelivery"
-            value={!formData.package.weekendDelivery}
-            onChange={(e) => handleChange(e, "package")}
-          />
-        </label>
+      <p>Destination address:</p>
+      <label>
+        street:
+        <input
+          type="text"
+          name="street"
+          value={formData.destinationAddress.street}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        house number:
+        <input
+          type="number"
+          name="houseNumber"
+          value={formData.destinationAddress.houseNumber}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        apartment number:
+        <input
+          type="number"
+          name="apartmentNumber"
+          value={formData.destinationAddress.apartmentNumber}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        city:
+        <input
+          type="text"
+          name="city"
+          value={formData.destinationAddress.city}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        postal code:
+        <input
+          type="text"
+          name="postalCode"
+          value={formData.destinationAddress.postalCode}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
+      <br />
+      <label>
+        country:
+        <input
+          type="text"
+          name="country"
+          value={formData.destinationAddress.country}
+          onChange={(e) => handleChange(e, "destinationAddress")}
+        />
+      </label>
 
-        <p>Source address</p>
+      <p>Delivery details:</p>
+      <label>
+        date:
+        <input
+          type="date"
+          name="deliveryDate"
+          value={formData.deliveryDate}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        priority:
         <label>
-          houseNumber:
           <input
-            type="text"
-            name="houseNumber"
-            value={formData.sourceAddress.houseNumber}
-            onChange={(e) => handleChange(e, "sourceAddress")}
+            type="radio"
+            name="priority"
+            value="option1"
+            checked={formData.priority}
+            onChange={(e) => handleChange(e)}
+            required
           />
+          high
         </label>
-        <br />
         <label>
-          apartmentNumber:
           <input
-            type="text"
-            name="apartmentNumber"
-            value={formData.sourceAddress.apartmentNumber}
-            onChange={(e) => handleChange(e, "sourceAddress")}
+            type="radio"
+            name="priority"
+            value="option2"
+            checked={!formData.priority}
+            onChange={(e) => handleChange(e)}
+            required
           />
+          low
         </label>
-        <br />
-        <label>
-          Street:
-          <input
-            type="text"
-            name="street"
-            value={formData.sourceAddress.street}
-            onChange={(e) => handleChange(e, "sourceAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          City:
-          <input
-            type="text"
-            name="city"
-            value={formData.sourceAddress.city}
-            onChange={(e) => handleChange(e, "sourceAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          Postal code:
-          <input
-            type="text"
-            name="postalCode"
-            value={formData.sourceAddress.postalCode}
-            onChange={(e) => handleChange(e, "sourceAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          Country:
-          <input
-            type="text"
-            name="country"
-            value={formData.sourceAddress.country}
-            onChange={(e) => handleChange(e, "sourceAddress")}
-          />
-        </label>
+      </label>
+      <br />
+      <label>
+        delivery at a weekend:
+        <input
+          type="checkbox"
+          name="weekendDelivery"
+          value={!formData.weekendDelivery}
+          onChange={(e) => handleChange(e)}
+        />
+      </label>
+      <br />
 
-        <p>Destination address</p>
-        <label>
-          houseNumber:
-          <input
-            type="text"
-            name="houseNumber"
-            value={formData.destinationAddress.houseNumber}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          apartmentNumber:
-          <input
-            type="text"
-            name="apartmentNumber"
-            value={formData.destinationAddress.apartmentNumber}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          Street:
-          <input
-            type="text"
-            name="street"
-            value={formData.destinationAddress.street}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          City:
-          <input
-            type="text"
-            name="city"
-            value={formData.destinationAddress.city}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          Postal code:
-          <input
-            type="text"
-            name="postalCode"
-            value={formData.destinationAddress.postalCode}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-        <br />
-        <label>
-          Country:
-          <input
-            type="text"
-            name="country"
-            value={formData.destinationAddress.country}
-            onChange={(e) => handleChange(e, "destinationAddress")}
-          />
-        </label>
-
-        <p>Delivery date</p>
-
-        <label>
-          Date:
-          <input
-            type="date"
-            name="deliveryDate"
-            value={formData.deliveryDate}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          IsVip:
-          <br />
-          <label>
-            <input
-              type="radio"
-              name="vipPackage"
-              value="option1"
-              checked={formData.vipPackage === true}
-              onChange={handleChange}
-              required
-            />
-            YES
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="vipPackage"
-              value="option2"
-              checked={formData.vipPackage === false}
-              onChange={handleChange}
-              required
-            />
-            NO
-          </label>
-        </label>
-        <br />
-        <label>
-          IsCompany:
-          <br />
-          <label>
-            <input
-              type="radio"
-              name="IsCompany"
-              value="option3"
-              checked={formData.isCompany === true}
-              onChange={handleChange}
-              required
-            />
-            YES
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="IsCompany"
-              value="option4"
-              checked={formData.isCompany === false}
-              onChange={handleChange}
-              required
-            />
-            NO
-          </label>
-        </label>
-
-        <br />
-        <button type="submit">Send delivery request</button>
-      </form>
+      <button onClick={handleSend}>Send delivery request</button>
     </div>
   );
 }
