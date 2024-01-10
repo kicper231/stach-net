5bdb2b2ff63142d16e7d174824c75668d57742d5
Stahson <stasiek.gwiazda0@gmail.com>
2024-01-10 17:04:18 +0100

    Połączenie z bazą danych CourierApiDatabase

---
+++ CourierApi/src/Infrastructure/Context.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ShopperContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ShopperContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+           .HasKey(u => u.UserId);
+            modelBuilder.Entity<User>()
+            .Property(u => u.FirstName)
+            .HasMaxLength(100);
+            modelBuilder.Entity<User>()
+                .Property(u => u.LastName);
+        }
+    }
+    
+
+}
