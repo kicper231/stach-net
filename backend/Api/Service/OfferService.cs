80dd6b8c05b01e347173b15bcf32cf57a056f3c8
Kapiszon <128547863+kicper231@users.noreply.github.com>
2024-01-10 20:08:55 +0100

    nie dziala ale blisko  // robienie polaczen

---
+++ backend/Api/Service/OfferService.cs
@@ -0,0 +1,30 @@
+﻿using Domain.DTO;
+using static System.Net.WebRequestMethods;
+
+namespace Api.Service;
+
+
+
+
+public class OfferService : IOfferService
+{
+    private readonly HttpClient _httpClientOffer;
+
+    public OfferService(HttpClient httpClient)
+    {
+        _httpClientOffer = httpClient;
+        //_httpClientOffer.BaseAddress= new Uri($"{Configuration["url"]}");
+    }
+
+    
+    public async Task<DeliveryRespondDTO> GetOffer(DeliveryRequestDTO requestDTO)
+    {
+        var response = await _httpClientOffer.PostAsJsonAsync("/" requestDTO);
+        //response.EnsureSuccessStatusCode();
+        //return await response.Content.ReadFromJsonAsync<DeliveryRespondDTO>();
+        return response;
+    }
+
+
+}
+
