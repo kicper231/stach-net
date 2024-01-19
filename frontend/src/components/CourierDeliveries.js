3587f949659db34e051b76f4ac0edd73c8644b79
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-01-19 18:11:02 +0100

    Add deliveries list for courier

--- frontend/src/components/CourierDeliveries.js
+++ frontend/src/components/CourierDeliveries.js
@@ -30,76 +30,104 @@ export function CourierDeliveries() {
     };
 
     getDeliveries();
-  }, [user.sub, getAccessTokenSilently]);
+  }, [getAccessTokenSilently]);
 
-  function InquiriesTable() {
-    const [addedInquiryId, setAddedInquiryId] = useState("");
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("Error");
-
-    const handleAdd = async () => {
-      try {
-        await axios.post(`${config.serverUri}/add-delivery`, {
-          deliveryID: addedInquiryId,
-          userAuth0: user.sub,
-        });
-
-        window.location.reload();
-      } catch (error) {
-        setIsError(true);
-        typeof error.response.data === "string" &&
-          setErrorMessage(error.response.data);
-        console.error(error);
-      }
-    };
-
+  function DeliveriesTable() {
     return (
       <div className="overflow">
         <h1>Inquiries</h1>
 
         <ul>
-          {deliveries.map((inquiry, index) => (
+          {deliveries.map((delivery, index) => (
             <li
               key={index}
-              className="inquiry"
+              className="delivery"
               onClick={() => navigate(`${index}`)}
             >
-              <strong>date:</strong> {inquiry.deliveryDate} /{" "}
-              <strong>source address:</strong> {inquiry.sourceAddress.street},{" "}
-              {inquiry.sourceAddress.city} /{" "}
+              <strong>date:</strong> {delivery.delivery.deliveryDate} /{" "}
+              <strong>source address:</strong>{" "}
+              {delivery.inquiry.sourceAddress.street},{" "}
+              {delivery.inquiry.sourceAddress.city} /{" "}
               <strong>destination address:</strong>{" "}
-              {inquiry.destinationAddress.street},{" "}
-              {inquiry.deliveryInfo && " / "}
-              {inquiry.deliveryInfo && <strong>status: </strong>}
-              {inquiry.deliveryInfo && inquiry.deliveryInfo.deliveryStatus}
+              {delivery.inquiry.destinationAddress.street},{" "}
+              {delivery.inquiry.destinationAddress.city},{" "}
+              <strong>status:</strong> {delivery.delivery.deliveryStatus}
             </li>
           ))}
         </ul>
-
-        <button onClick={handleAdd}>Add inquiry</button>
-        <br />
-        <input
-          type="text"
-          value={addedInquiryId}
-          placeholder="added inquiry id"
-          onChange={(e) => setAddedInquiryId(e.target.value)}
-        />
-        <br />
-        {isError && <h1 className="red">{errorMessage}</h1>}
       </div>
+    );
+  }
+
+  function Delivery({ delivery }) {
+    return (
+      <>
+        <div className="contexHolder">
+          <h1>Delivery</h1>
+          <ul>
+            <li>
+              <strong>id:</strong> {delivery.delivery.deliveryId}
+            </li>
+            <li>
+              <strong>cost:</strong> {delivery.offer.totalPrice}{" "}
+              {delivery.offer.currency}
+            </li>
+            {delivery.user && (
+              <li>
+                <strong>user:</strong> {delivery.user.firstName}{" "}
+                {delivery.user.lastName}, {delivery.user.email}
+              </li>
+            )}
+            <li>
+              <strong>package dimensions:</strong>{" "}
+              {delivery.inquiry.package.width}m x{" "}
+              {delivery.inquiry.package.height}m x{" "}
+              {delivery.inquiry.package.length}m
+            </li>
+            <li>
+              <strong>package weight:</strong> {delivery.inquiry.package.weight}
+              kg
+            </li>
+            <li>
+              <strong>source address:</strong>
+              <br />
+              {displayAddress(delivery.inquiry.sourceAddress)}
+            </li>
+            <li>
+              <strong>destination address:</strong>
+              <br />
+              {displayAddress(delivery.inquiry.destinationAddress)}
+            </li>
+            <li>
+              <strong>delivery date:</strong> {delivery.inquiry.deliveryDate}
+            </li>
+            <li>
+              <strong>priority:</strong>{" "}
+              {delivery.inquiry.priority ? "yes" : "no"}
+            </li>
+            <li>
+              <strong>weekend delivery:</strong>{" "}
+              {delivery.inquiry.weekendDelivery ? "yes" : "no"}
+            </li>
+            <li>
+              <strong>status:</strong> {delivery.delivery.deliveryStatus}
+            </li>
+          </ul>
+        </div>
+      </>
     );
   }
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<InquiriesTable />} />
+        <Route path="/" element={<DeliveriesTable />} />
 
-        {deliveries.map((inquiry, index) => (
+        {deliveries.map((delivery, index) => (
           <Route
             key={index}
             path={`${index}`}
-            element={<Inquiry inquiry={inquiry} />}
+            element={<Delivery delivery={delivery} />}
           />
         ))}
       </Routes>
@@ -107,56 +135,13 @@ export function CourierDeliveries() {
   );
 }
 
-function Inquiry({ inquiry }) {
+function displayAddress(address) {
   return (
     <>
-      <div className="contexHolder">
-        <h1>Inquiry</h1>
-        <ul>
-          <li>
-            <strong>package dimensions:</strong> {inquiry.package.width}m x{" "}
-            {inquiry.package.height}m x {inquiry.package.length}m
-          </li>
-          <li>
-            <strong>package weight:</strong> {inquiry.package.weight}kg
-          </li>
-
-          <li>
-            <strong>source address:</strong>
-            <br />
-            {inquiry.sourceAddress.street} {inquiry.sourceAddress.houseNumber}
-            {inquiry.sourceAddress.apartmentNumber &&
-              " / " + inquiry.sourceAddress.apartmentNumber}
-            <br />
-            {inquiry.sourceAddress.city} {inquiry.sourceAddress.zipCode},{" "}
-            {inquiry.sourceAddress.country}
-          </li>
-
-          <li>
-            <strong>source address:</strong>
-            <br />
-            {inquiry.destinationAddress.street}{" "}
-            {inquiry.destinationAddress.houseNumber}
-            {inquiry.destinationAddress.apartmentNumber &&
-              " / " + inquiry.destinationAddress.apartmentNumber}
-            <br />
-            {inquiry.destinationAddress.city}{" "}
-            {inquiry.destinationAddress.zipCode},{" "}
-            {inquiry.destinationAddress.country}
-          </li>
-
-          <li>
-            <strong>delivery date:</strong> {inquiry.deliveryDate}
-          </li>
-          <li>
-            <strong>priority:</strong> {inquiry.priority ? "yes" : "no"}
-          </li>
-          <li>
-            <strong>weekend delivery:</strong>{" "}
-            {inquiry.weekendDelivery ? "yes" : "no"}
-          </li>
-        </ul>
-      </div>
+      {address.street} {address.houseNumber}{" "}
+      {address.apartmentNumber && " / " + address.apartmentNumber},
+      <br />
+      {address.city} {address.zipCode}, {address.country}
     </>
   );
 }
